8609cdd2aa7ae151187b6db2904877299377e4f6
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-20 17:06:31 +0200

    Aggiunto calcolo prezzo per fascia età

--- js/main.js
+++ js/main.js
@@ -6,19 +6,39 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
 
+/* Informazioni utili */
+const underPriceAge = 18;
+const overPriceAge = 65;
+let lastPrice = ``;
+console.log(underPriceAge, overPriceAge, lastPrice);
+
 /* Chiedi all'utente il numero di chilometri che vuole percorrere */
-const numberKm = Number(prompt("Quanti km vuoi percorrere?"));
-console.log(numberKm);
+let tripLenght = prompt(`Inserisci i km del tuo percorso`)
+console.log(tripLenght);
 
 /* Chiedi all'utente la sua età */
-const age = Number(prompt("Scrivi la tua età"));
+let age = Number(prompt(`Inserisci la tua età`));
 console.log(age)
 
 /* il prezzo del biglietto è definito in base ai km (0.21 € al km) */
-const priceTicket = (0.21 * numberKm);
-console.log(priceTicket);
+let tripCostKm = 0.21;
+console.log(tripCostKm);
+let firstPrice = tripLenght * tripCostKm;
+console.log(firstPrice);
 
-let ( age => 18 )
-if (priceTicket * 20 / 100) {
-} 
-console.log(priceTicket)
+/* Prezzo finale scontato in base alla fascia di età */
+let underPrice = (firstPrice * 20) / 100;
+console.log(underPrice);
+let overPrice = (firstPrice * 40) / 100;
+console.log(overPrice);
+
+/* Applichiamo lo sconto */
+if (age < underPriceAge){
+    lastPrice = firstPrice - underPrice;
+} else if (age > overPriceAge){
+    lastPrice = firstPrice - overPrice;
+} else {
+    lastPrice = firstPrice - 0;
+}
+
+
